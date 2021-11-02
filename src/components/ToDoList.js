3b2157e941fd557b1de21cdf629905c5ecdc666e
nofar5353 <48683480+nofar5353@users.noreply.github.com>
2021-11-02 15:36:09 +0200

    first commit

---
+++ src/components/ToDoList.js
@@ -0,0 +1,19 @@
+import '../css/ToDoList.css';s
+
+function ToDoList() {
+    return (
+      <div class="toDoList-container">
+        <div class="listHeader-section">
+        <h1>TO DO</h1>
+        </div>
+        <div class="listItem-section">
+
+        </div>
+        <div class="insertItem-section">
+
+        </div>
+      </div>
+    );
+  }
+  
+  export default ToDoList;
